a7dc87ebb271f950889c3936cf688a7a000a951b
Mohammed Ahmed <mohammed.mo.alkabbani@gmail.com>
2026-06-12 02:08:16 +0300

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n/request.ts');
 
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['firebase-admin'],
   images: {
     remotePatterns: [
       {
